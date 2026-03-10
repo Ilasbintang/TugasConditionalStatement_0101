1e7110dbab863541f811575592d23521611b7d2e
Ilasbintang <ilasbintang727@gmail.com>
2026-03-10 15:45:30 +0800

    commit 2

--- tugasbmi.cpp
+++ tugasbmi.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 
-
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
 
 
 
