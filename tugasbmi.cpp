69b177554713ee50e398874d6bab57ddc45e5530
Ilasbintang <ilasbintang727@gmail.com>
2026-03-10 15:47:22 +0800

    commit 5

--- tugasbmi.cpp
+++ tugasbmi.cpp
@@ -30,3 +30,14 @@ string statusBMI(float bmi) {
     }
 }
 
+int main() {
+    float berat, tinggi, bmi;
+
+    cout << "--- Selamat Datang Mahasiswa Kelas E 2026 ---" << endl;
+
+   
+    inputData(berat, tinggi);
+
+  
+   
+}
